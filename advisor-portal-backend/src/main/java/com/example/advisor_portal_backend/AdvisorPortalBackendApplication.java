04bdeca45613b6156f3b76d949669ccc1db71df3
Swapnil <stamsepublic@gmail.com>
2025-02-23 19:57:44 -0500

    Add springboot backend

---
+++ advisor-portal-backend/src/main/java/com/example/advisor_portal_backend/AdvisorPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.advisor_portal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisorPortalBackendApplication.class, args);
+	}
+
+}
